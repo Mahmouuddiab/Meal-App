0a293058d5a85108a91e2161d6441ff05c95cff0
Mahmouuddiab <diabmahmoud450@gmail.com>
2025-08-03 16:05:58 +0300

    feature1 register done

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:meal_app/core/locator/service_locator.dart';
+import 'package:meal_app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:meal_app/features/auth/presentation/screens/register.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
+
 void main()async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Supabase.initialize(
-      url: "https://sayzgcursvgjkpbrcotm.supabase.co",
-      anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNheXpnY3Vyc3ZnamtwYnJjb3RtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQxOTkwMTgsImV4cCI6MjA2OTc3NTAxOH0.7UVImAs1t2ftXQ7o14_lYnxWuLLDyVMkaL9vRIJPSeg");
-  runApp(const MyApp());
+  await initDependices();
+  runApp(
+      MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (context) => serviceLocator<AuthBloc>(),)
+        ],
+          child: const MyApp()
+      )
+  );
 }
 
 class MyApp extends StatelessWidget {
