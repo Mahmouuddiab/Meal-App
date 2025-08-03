bceb9e67efa2565be4359f3b2103695f5660ef4a
rofidarashed <rofidaelmaghraby@gmail.com>
2025-08-03 14:45:14 +0300

    Add images and update color and string utilities

---
+++ lib/core/Utils/app_colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class AppColors {
+  static Color mainColor = Color(0xff001A3F);
+  static Color white = Color(0xffffffff);
+  static Color black = Color(0xff000000);
+  static Color gray = Color(0xff8A8A8A);
+  static Color unselectedGray = Color(0xffADADAD);
+  static Color borderGray = Color(0xffDEDEDE);
+  static Color textGray = Color(0xff767676);
+  static Color starColor = Color(0xffFFD32C);
+}
---
+++ lib/core/Utils/app_images.dart
@@ -0,0 +1,8 @@
+class AppImages {
+  static String logo = "assets/images/logo.png";
+  static String onboarding1 = "assets/images/onboarding1.png";
+  static String onboarding2 = "assets/images/onboarding2.png";
+  static String google = "assets/images/google.png";
+  static String facebook = "assets/images/facebook.png";
+  static String authBackgroung = "assets/images/auth_background.png";
+}
